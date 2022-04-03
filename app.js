10cc4da674c9541065b70a3454a6bfe1441ca894
pp-zbigz <pasuk.phonsuphee@gmail.com>
2022-04-04 00:38:51 +0700

    update#1

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/apis/restaurants', (req, res) => {
+    res.send('GET API');
+});
+
+app.get('/apis/restaurants/:id', (req, res) => {
+    res.send('GET ONE API');
+});
+
+app.post('/apis/restaurants', (req, res) => {
+    res.send('POST API');
+});
+
+app.put('/apis/restaurants/:id', (req, res) => {
+    res.send('PUT API');
+});
+app.delete('/apis/restaurants', (req, res) => {
+    res.send('DELETE API');
+});
+
 app.get('/about', (req, res) => {
     res.send('About Page');
 });
