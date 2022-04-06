8f6a92eafa18fe0f6b135cedc028a1ce39bbac91
pp-zbigz <pasuk.phonsuphee@gmail.com>
2022-04-06 23:29:26 +0700

    add data #update3

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        id: 1,
+        name: "Subway"
+    },
+    {
+        id: 2,
+        name: "Pad Thai"
+    },
+    {
+        id: 3,
+        name: "BonChon"
+    }
+]
